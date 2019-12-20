1fd9d85bf1560fb7fdfb35158a6895ca249eb907
ljq <lijianqiao@tydic.com>
2019-12-20 09:51:40 +0800

    景区系统管理后台页面项目初始化，登录功能完成

---
+++ src/components/cms/slideDetails.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="ljq">
+        <h1 class="el-button" >基本信息</h1>
+        <br>
+        <br>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            标题：{{ $route.params.data.title }}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            创建人：{{ $route.params.data.createUserName }}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            创建时间：{{ $route.params.data.gmtCreate }}</p><br><br>
+
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            显示位置：{{ position }}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            备注：{{ $route.params.data.remark }}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            状态：{{ isEnable }}</p>
+        <br>
+        <br>
+        <h1 class="el-button" >图片信息</h1>
+        <br>
+        <br>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            是否显示内容：{{ isShowContent }}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            内容：{{ $route.params.data.content }}
+        </p>
+        <br>
+        <br>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            是否可跳转：{{ isLink }}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            详情url：{{ $route.params.data.linkUrl }}
+        </p>
+        <br>
+        <br>
+        <h1 class="el-button" >缩略图</h1>
+        <div class="demo-image__placeholder">
+
+<!--            {{ this.$route.params.data.imageUrl }}-->
+            <div class="block">
+                <span class="demonstration"></span>
+                <el-image :src="src"></el-image>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "slideDetails",
+        data () {
+            return{
+                position:this.toPosition(this.$route.params.data.position),
+                isEnable:this.toEnable(this.$route.params.data.isEnable),
+                isShowContent:this.toIsShowContent(this.$route.params.data.isShowContent),
+                isLink:this.toIsShowContent(this.$route.params.data.isLink),
+                // src:this.$route.params.data.imageUrl
+                src:"http://www.tomqiao.cn/timg.jpg"
+            }
+        },
+        methods:{
+            toPosition(positon){
+                if (positon === 2) {
+                    return '启动页';
+                }
+                if (positon === 1) {
+                    return '登陆页';
+                }
+                if (positon === 0) {
+                    return '首页';
+                }
+            },
+            toEnable(i){
+                if (i === 1) {
+                    return '启用';
+                }
+                if (i === 0) {
+                    return '禁用';
+                }
+            },
+            toIsShowContent(i){
+                if (i === 1) {
+                    return '是';
+                }
+                if (i === 0) {
+                    return '否';
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+.ljq{
+    width: 1300px;
+    height: 100%;
+    background-color: white;
+    margin-left: 150px;
+    padding: 50px;
+    font-size: large;
+}
+</style>
---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="sidebar">
+        <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#324157"
+            text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
+            <template v-for="item in items">
+                <template v-if="item.subs">
+                    <el-submenu :index="item.index" :key="item.index">
+                        <template slot="title">
+                            <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
+                        </template>
+                        <template v-for="subItem in item.subs">
+                            <el-submenu v-if="subItem.subs" :index="subItem.index" :key="subItem.index">
+                                <template slot="title">{{ subItem.title }}</template>
+                                <el-menu-item v-for="(threeItem,i) in subItem.subs" :key="i" :index="threeItem.index">
+                                    {{ threeItem.title }}
+                                </el-menu-item>
+                            </el-submenu>
+                            <el-menu-item v-else :index="subItem.index" :key="subItem.index">
+                                {{ subItem.title }}
+                            </el-menu-item>
+                        </template>
+                    </el-submenu>
+                </template>
+                <template v-else>
+                    <el-menu-item :index="item.index" :key="item.index">
+                        <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
+                    </el-menu-item>
+                </template>
+            </template>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    import bus from '../common/bus';
+    export default {
+        data() {
+            return {
+                collapse: false,
+                items: [
+                    {
+                        icon: 'el-icon-lx-cascades',
+                        index: '1',
+                        title: '内容管理',
+                        subs: [
+                            {
+                                index: 'slides',
+                                title: '轮播管理'
+                            },
+                            {
+                                index: 'charts',
+                                title: '通知公告'
+                            }]
+
+                    },
+                    {
+                        icon: 'el-icon-lx-calendar',
+                        index: '2',
+                        title: '日志管理',
+                        subs: [
+                            {
+                                index: 'tabs',
+                                title: '登陆日志'
+                            }]
+
+                    },
+                    {
+                        icon: 'el-icon-lx-settings',
+                        index: '3',
+                        title: '系统管理',
+                        subs: [
+                            {
+                                index: 'upload',
+                                title: '用户管理'
+                            },
+                            {
+                                index: 'markdown',
+                                title: '角色权限管理'
+                            },
+                            {
+                                index: 'editor',
+                                title: '通讯录'
+                            },
+                            {
+                                index: 'form',
+                                title: '意见查看'
+                            }]
+
+                    }
+                ]
+            }
+        },
+        computed:{
+            onRoutes(){
+                return this.$route.path.replace('/','');
+            }
+        },
+        created(){
+            // 通过 Event Bus 进行组件间通信，来折叠侧边栏
+            bus.$on('collapse', msg => {
+                this.collapse = msg;
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .sidebar{
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 70px;
+        bottom:0;
+        overflow-y: scroll;
+    }
+    .sidebar::-webkit-scrollbar{
+        width: 0;
+    }
+    .sidebar-el-menu:not(.el-menu--collapse){
+        width: 250px;
+    }
+    .sidebar > ul {
+        height:100%;
+    }
+</style>
