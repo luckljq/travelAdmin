61451458f98771b654a5880781485fcdefc2d7a2
ljq <lijianqiao@tydic.com>
2019-12-24 11:25:46 +0800

    启用/禁用用户功能

--- src/utils/request.js
+++ src/utils/request.js
@@ -20,21 +20,30 @@ service.interceptors.request.use( config => {
     }
     return config;
 }, error => {
-    Message.error("请求超时");
+    Message.error({
+        message: "请求超时",
+        center:true
+    });
     console.log(error);
     return Promise.reject();
 });
 
 service.interceptors.response.use(response => {
     if(response.status === 200 && response.data.code === -1){
-        Message.error({message: response.data.message});
+        Message.error({
+            message: response.data.message,
+            center:true
+        });
         return Promise.reject();
     }else{
         return response.data;
     }
 }, error => {
     console.log(error);
-    Message.error({message: error.message});
+    Message.error({
+        message: error.message,
+        center:true
+    });
     return Promise.reject();
 });
 
