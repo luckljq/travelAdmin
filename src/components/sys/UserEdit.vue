7451508311360486c5df143039668fbe71cf23d5
ljq <lijianqiao@tydic.com>
2019-12-24 17:54:04 +0800

    编辑页面

--- src/components/sys/UserEdit.vue
+++ src/components/sys/UserEdit.vue
@@ -8,7 +8,30 @@
             </el-breadcrumb>
         </div>
         <div class="container">
-        {{$route.params.data}}
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="用户昵称" prop="name">
+                    <el-input v-model="ruleForm.name" style="width: 215px"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域" prop="region">
+                    <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="用户性别" prop="sex">
+                    <el-radio-group v-model="ruleForm.sex">
+                        <el-radio label="男"></el-radio>
+                        <el-radio label="女"></el-radio>
+                        <el-radio label="保密"></el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="活动形式" prop="desc">
+                    <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" >确认修改</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
@@ -17,6 +40,28 @@
         name: "userEdit",
         data () {
             return{
+                user:this.$route.params.data,
+                ruleForm: {
+                    name: this.$route.params.data.userName,
+                    region: '',
+                    sex: '',
+                    desc: ''
+                },
+                rules: {
+                    name: [
+                        { required: true, message: '请输入活动名称', trigger: 'blur' },
+                        { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                    ],
+                    region: [
+                        { required: true, message: '请选择活动区域', trigger: 'change' }
+                    ],
+                    sex: [
+                        { required: true, message: '请选择性别', trigger: 'change' }
+                    ],
+                    desc: [
+                        { required: true, message: '请填写活动形式', trigger: 'blur' }
+                    ]
+                }
             }
         },
         methods:{
